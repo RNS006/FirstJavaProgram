6cb5d42d38f73fb3b29523fcfc25dc029a07babb
RNS006 <rudra10gulyam@gmail.com>
2021-03-18 22:20:29 +0530

    added TestDemo1 program

---
+++ TestDemo1.java
@@ -0,0 +1,12 @@
+public class TestDemo1{
+     public static void main(String[] args){
+       
+        for(int i=1; i<=10; i++){
+
+        System.out.println("Hello World!..."+i);
+		System.out.println("Above program is successfully executed...!");
+}
+
+}
+
+}
